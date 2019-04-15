aa5a3d9f96a7fb153d8cccdbfcf8ce7f704376b0
Johan Garcia <john.jgp@gmail.com>
2019-04-15 00:12:52 -0500

    messed with styles a little, created the order details view, need to try the bottom sheet with mvrx

--- app/src/main/java/com/treefuerza/simplepos/ui/main/orders/create/CreateOrderFragment.kt
+++ app/src/main/java/com/treefuerza/simplepos/ui/main/orders/create/CreateOrderFragment.kt
@@ -9,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.treefuerza.simplepos.R
+import com.treefuerza.simplepos.utils.RoundedBottomSheetDialogFragment
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,18 +25,10 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  *
  */
-class CreateOrderFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-    private var listener: OnFragmentInteractionListener? = null
+class CreateOrderFragment : RoundedBottomSheetDialogFragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    override fun invalidate() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun onCreateView(
@@ -46,58 +39,12 @@ class CreateOrderFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_create_order, container, false)
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    fun onButtonPressed(uri: Uri) {
-        listener?.onFragmentInteraction(uri)
-    }
-
     override fun onAttach(context: Context) {
         super.onAttach(context)
-        if (context is OnFragmentInteractionListener) {
-            listener = context
-        } else {
-            throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
-        }
+
     }
 
     override fun onDetach() {
         super.onDetach()
-        listener = null
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     *
-     *
-     * See the Android Training lesson [Communicating with Other Fragments]
-     * (http://developer.android.com/training/basics/fragments/communicating.html)
-     * for more information.
-     */
-    interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        fun onFragmentInteraction(uri: Uri)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CreateOrderFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CreateOrderFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
---
+++ app/src/main/java/com/treefuerza/simplepos/utils/RoundedBottomSheetDialogFragment.kt
@@ -0,0 +1,52 @@
+package com.treefuerza.simplepos.utils
+
+import android.app.Dialog
+import android.os.Bundle
+import androidx.lifecycle.LifecycleOwner
+import com.airbnb.mvrx.MvRxView
+import com.airbnb.mvrx.MvRxViewModelStore
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.treefuerza.simplepos.R
+import java.util.*
+
+abstract class RoundedBottomSheetDialogFragment : BottomSheetDialogFragment(), MvRxView {
+
+    override fun getTheme(): Int = R.style.BottomSheetDialogTheme
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog = BottomSheetDialog(requireContext(), theme)
+
+    override val mvrxViewModelStore by lazy { MvRxViewModelStore(viewModelStore) }
+
+    final override val mvrxViewId: String by lazy { mvrxPersistedViewId }
+
+    private lateinit var mvrxPersistedViewId: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        mvrxViewModelStore.restoreViewModels(this, savedInstanceState)
+        mvrxPersistedViewId = savedInstanceState?.getString(PERSISTED_VIEW_ID_KEY) ?: this::class.java.simpleName + "_" + UUID.randomUUID().toString()
+        super.onCreate(savedInstanceState)
+    }
+
+    /**
+     * Fragments should override the subscriptionLifecycle owner so that subscriptions made after onCreate
+     * are properly disposed as fragments are moved from/to the backstack.
+     */
+    override val subscriptionLifecycleOwner: LifecycleOwner
+        get() = this.viewLifecycleOwnerLiveData.value ?: this
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mvrxViewModelStore.saveViewModels(outState)
+        outState.putString(PERSISTED_VIEW_ID_KEY, mvrxViewId)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // This ensures that invalidate() is called for static screens that don't
+        // subscribe to a ViewModel.
+        postInvalidate()
+    }
+}
+
+private const val PERSISTED_VIEW_ID_KEY = "mvrx:persisted_view_id"
